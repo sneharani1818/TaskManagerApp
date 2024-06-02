d8f4a4bcf06d953dce37043e3c281db066c31f8f
sneharani1818 <sneharani1818@gmail.com>
2024-06-02 16:52:35 +0530

    Successful user registration

--- index.php
+++ index.php
@@ -17,35 +17,41 @@
     $email=$_POST['email'];
     $password=$_POST['password'];
     $full_name=$_POST['full_name'];
-    if (isset($_POST['profile_img'])) {
-        $profile_img = $_POST['profile_img'];
-    } else {
-        $profile_img = null; // or handle it as needed
-    }
     if (isset($_POST['phno'])) {
         $phno = $_POST['phno'];
     } else {
         $phno = ''; // or handle it as needed
     }
     $dob=$_POST['dob'];
-    $address=$_POST['address'];
     $ques_type=$_POST['ques_type'];
+    if($ques_type=="What was the name of your first pet?")
+        $ques_type="pet";
+    else if($ques_type=="What is your mother's fav food name?")
+        $ques_type="food";
+    else 
+        $ques_type="school";
+
     $ans_of_ques=$_POST['ans_of_ques'];
-    // $curr_time=$_POST[curr_time];
-    
+
     echo $email."<br>";
     echo $password."<br>";
     echo $full_name."<br>";
-    echo $profile_img."<br>";
-    echo "<img src=".$profile_img.">";
     echo $phno."<br>";
     echo $dob."<br>";
-    echo $address."<br>";
     echo $ques_type."<br>";
     echo $ans_of_ques;
 
-    // $sql="INSERT INTO `users` (`email`, `password`, `full_name`, `profile_img`, `phno`, `dob`, `address`, `ques_type`, `ans_of_ques`, `curr_time`) VALUES ('abc@gmail.com', 'password', 'John Delta', NULL, '9999888877', '22-04-2002', 'London', 'pet', 'Cow',current_timestamp());";
+    // $sql="INSERT INTO `users` (`email`, `password`, `full_name`, `phno`, `dob`, `ques_type`, `ans_of_ques`, `curr_time`) VALUES (`$email`, `$password`, `$full_name`, `$phno`, `$dob`, `$ques_type`, `$ans_of_ques`,current_timestamp());";
+    $sql = "INSERT INTO `users` (`email`, `password`, `full_name`, `phno`, `dob`, `ques_type`, `ans_of_ques`, `curr_time`) VALUES ('$email', '$password', '$full_name', '$phno', '$dob', '$ques_type', '$ans_of_ques', current_timestamp());";
     
+    //executing query
+    if($con->query($sql)){
+        echo "Record inserted";
+    }
+    else{
+        echo "Not inserted: Error =".$con->error;
+    }
+
     mysqli_close($con);
 
 ?>
